f4b6bd8e4cf5eaf9f9904f476885eb60e9e8297c
Sojus2021 <86943278+Sojus2021@users.noreply.github.com>
2021-10-23 23:11:40 -0400

    Complete portfolio

---
+++ index.js
@@ -0,0 +1,14 @@
+
+
+const about = document.querySelector('.about');
+const projects = document.querySelector('.projects');
+
+about.addEventListener('click', (e) => {
+    e.preventDefault();
+    
+
+})
+projects.addEventListener('click', (e) => {
+    e.preventDefault();
+
+})
